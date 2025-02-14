de1c76fe8eca20d8f2c4c0fab1b64d8f36ecb124
SowaOwl <savvalaptev3@gmail.com>
2025-02-14 14:52:16 +0500

    Finish library client

--- library/library.go
+++ library/library.go
@@ -1,1 +1,52 @@
 package library
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"github.com/tiaguinho/gosoap"
+	"net/http"
+	"os"
+)
+
+type Client struct {
+	http *http.Client
+}
+
+func NewClient() *Client {
+	certPath := "new_cert.pem"
+
+	caCert, err := os.ReadFile(certPath)
+	if err != nil {
+		panic(err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: true,
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+
+	return &Client{
+		http: &http.Client{Transport: transport},
+	}
+}
+
+func (c *Client) SendRequest(url string, methodName string, data map[string]interface{}) ([]byte, error) {
+	client, err := gosoap.SoapClient(url, c.http)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Call(methodName, data)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
